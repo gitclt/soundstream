92139343c30c97aa0866771c20ed052d3bb4aef9
hudafathimakp <101244047+hudafathimakp@users.noreply.github.com>
2024-03-21 10:51:38 +0530

    main

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -2,6 +2,16 @@ import 'package:get/get.dart';
 
 class HomeController extends GetxController {
 
+ var selectedIndex = 0.obs;
 
-
+  void selectItem(int index) {
+    selectedIndex.value = index;
+  }
+ 
+  final List<String> option = [
+    'All',
+    'Speeches',
+    ' Songs',
+   
+  ];
 }
--- lib/app/modules/home/views/home_start_view.dart
+++ lib/app/modules/home/views/home_start_view.dart
@@ -5,6 +5,7 @@ import 'package:sound_stream_flutter_app/app/modules/home/views/home_view.dart';
 import 'package:sound_stream_flutter_app/common_widgets/svg_widget/svg_widget.dart';
 
 import 'package:sound_stream_flutter_app/common_widgets/text/text.dart';
+import 'package:sound_stream_flutter_app/constrains/app_color.dart';
 import '../controllers/home_controller.dart';
 
 class StartView extends GetView<HomeController> {
@@ -24,11 +25,10 @@ class StartView extends GetView<HomeController> {
             ),
             SliverFillRemaining(
                 hasScrollBody: true,
-                child: ListView.builder(
+                child: ListView(
                   shrinkWrap: true,
-                  itemCount: 1,
-                  itemBuilder: (context, index) {
-                    return Padding(
+                  children: [
+                    Padding(
                       padding:
                           const EdgeInsets.only(top: 50, left: 20, right: 20),
                       child: Column(
@@ -68,11 +68,64 @@ class StartView extends GetView<HomeController> {
                           blackText('Categories', 20,
                                   fontWeight: FontWeight.w700)
                               .paddingSymmetric(vertical: 20),
+                          SizedBox(
+                              height: 40,
+                              child: ListView.builder(
+                                itemCount: 3,
+                                scrollDirection: Axis.horizontal,
+                                itemBuilder: (context, index) {
+                                  return Obx(() => CustomListTile(
+                                        title: controller.option[index],
+                                        onTap: () =>
+                                            controller.selectItem(index),
+                                        isSelected:
+                                            controller.selectedIndex.value ==
+                                                index,
+                                      ));
+                                },
+                              )),
                           const CategoryCard(),
+
+                          //  ListView.builder(
+                          //     scrollDirection: Axis.horizontal,
+                          //     itemCount: 3,
+                          //     itemBuilder: (index, selectedIndex) {
+                          //       return Padding(
+                          //         padding: const EdgeInsets.all(5),
+                          //         child: Container(
+                          //           padding: EdgeInsets.all(8),
+                          //           decoration: BoxDecoration(
+                          //             color: Colors.white,
+                          //             borderRadius: const BorderRadius.all(
+                          //                 Radius.circular(25)),
+                          //             border: Border.all(width: 0.5),
+                          //           ),
+                          //           height: selectedIndex == index ? 40 : 20,
+                          //           child: blackText("All", 12),
+                          //         ),
+                          //       ) ;
+                          //     }),
+
+                          //    ListView.builder(
+                          //       scrollDirection: Axis.horizontal,
+                          //       itemCount: 3,
+                          //       itemBuilder: (context, index) {
+                          //         return Container(
+                          //           padding: EdgeInsets.all(8),
+                          //           child: blackText("All", 12),
+                          //           decoration: BoxDecoration(
+                          //             color: Colors.white,
+                          //             borderRadius: const BorderRadius.all(
+                          //                 Radius.circular(25)),
+                          //             border: Border.all(width: 0.5),
+                          //           ),
+                          //         );
+                          //       }),
+                          // ),
                         ],
                       ),
-                    );
-                  },
+                    ),
+                  ],
                 )),
           ],
         ),
@@ -118,5 +171,63 @@ class CategoryCard extends StatelessWidget {
         ],
       ),
     );
+  }
+}
+
+class CustomListTile extends StatelessWidget {
+  final String title;
+  final Function onTap;
+  final bool isSelected;
+
+  CustomListTile({
+    required this.title,
+    required this.onTap,
+    required this.isSelected,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap as void Function()?,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 4),
+          decoration: BoxDecoration(
+            gradient: isSelected
+                ? primaryColor
+                : const LinearGradient(
+                    begin: Alignment.centerLeft,
+                    end: Alignment.centerRight,
+                    colors: [Colors.white, Colors.white],
+                  ),
+            // color: isSelected ? Colors.blue : Colors.black,
+            borderRadius: const BorderRadius.all(Radius.circular(25)),
+            border: Border.all(width: 0.5),
+          ),
+          // height: selectedIndex == index ? 40 : 20,
+          child: Text(
+            title,
+            style: TextStyle(
+              fontSize: 14,
+              color: isSelected ? Colors.white : Colors.black,
+            ),
+          ),
+        ),
+      ),
+    );
+
+    // ListTile(
+    //   title:
+    //   Text(
+    //     title,
+    //     style: TextStyle(
+    //       color: isSelected
+    //           ? Colors.blue
+    //           : Colors.black, // Change color based on selection
+    //     ),
+    //   ),
+    //   onTap: onTap as void Function()?,
+    // );
   }
 }
