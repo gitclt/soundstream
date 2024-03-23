f7e5bed81fc55c4a7f85ab107373de15a9f56baf
hudafathimakp <101244047+hudafathimakp@users.noreply.github.com>
2024-03-23 14:44:23 +0530

    main

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -2,9 +2,13 @@ import 'dart:convert';
 
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:get/get.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:sound_stream_flutter_app/app/model/song_model.dart';
+import 'package:sound_stream_flutter_app/common_widgets/popup/dialog_helper.dart';
+import 'package:sound_stream_flutter_app/constrains/service/location.dart';
 
 class HomeController extends GetxController with GetTickerProviderStateMixin {
   late TabController mainController;
@@ -89,4 +93,72 @@ class HomeController extends GetxController with GetTickerProviderStateMixin {
           .where((element) => names.contains(element.split("/").last)));
     }
   }
+
+
+Future<bool> getCurrentPos(Position position) async {
+    try {
+      crlatitude = position.latitude.toString();
+      crlongitude = position.longitude.toString();
+
+      List<Placemark> coordinates = await placemarkFromCoordinates(
+          double.parse(crlatitude), double.parse(crlongitude));
+
+      Placemark geoAddress = coordinates.first;
+      place = geoAddress.locality!;
+      locality = geoAddress.subLocality!;
+
+      return true;
+    } catch (e) {
+      //  print(e.toString());
+      return false;
+    }
+  }
+
+//  Future<double> getLocation(
+//     double lati,
+//     double longi,
+//   ) async {
+//     // if (value != 1) {
+//     DialogHelper.showLoading("Fetching Location ...");
+//     final location = await determinePosition();
+//     DialogHelper.hideLoading();
+//     if (location == null) return 0.0;
+
+//     final locStatus = await getCurrentPos(location);
+
+//     if (!locStatus) return 0.0;
+
+//     var distance = checkDistance(
+//       lati,
+//       longi,
+//       double.parse(crlatitude),
+//       double.parse(crlongitude),
+//     );
+
+//     return distance;
+//   }
+
+
+   Future<void> fetchLocation(
+    // BuildContext context,
+    // bool status,
+    // String kmDiff,
+    // String visitType,
+    // String empId,
+    // String leadId,
+    // String place,
+  ) async {
+    // itemController.clear();
+    // planChecked.value = false;
+    // creditChecked.value = false;
+    // remarkController.clear();
+    DialogHelper.showLoading("Fetching Location ...");
+    final location = await determinePosition();
+    DialogHelper.hideLoading();
+    if (location == null) return;
+
+    final locStatus = await getCurrentPos(location);
+
+    if (!locStatus)return;
+}
 }
